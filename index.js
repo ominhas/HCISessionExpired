fb57a0d67102768031dd54d690b2bb9da229bc0f
ominhas <minhao@rpi.edu>
2018-04-21 18:06:24 -0400

    Made countdown
    yay

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 var app = angular.module('sessionExpired' ,[]);
-app.controller('sessionController', function($scope, $http, $timeout) {
+app.controller('sessionController', function($scope, $http, $timeout, $interval) {
 	$scope.auth = false;
 	$scope.time = 5000;
 	$scope.si = true;
 	$scope.inp = 5;
 	$scope.logger = "ng-show";
 	$scope.logged = "ng-hide";
+	$scope.counter = "";
 	$scope.login = function() {
-		alert("You've logged in!");
 		$scope.auth = true;
 		$scope.logger = "ng-hide";
 		$scope.logged = "ng-show";
+		alert("You've logged in!");
 	}
 	$scope.expire = function() {
-		alert("Your session has expired!");
 		$scope.auth = false;
 		$scope.logout();
+		alert("Your session has expired!");
 	}
 	$scope.update = function() {
-		alert("Updated!");
 		$scope.time = $scope.inp*1000;
 		if ($scope.deactivate) {
 			$scope.si = false;
 			$timeout.cancel($scope.timer);
 		}
+		alert("Updated!");
 	}
 	$scope.logout = function() {
 		$timeout.cancel($scope.timer);
@@ -31,10 +32,20 @@ app.controller('sessionController', function($scope, $http, $timeout) {
 		$scope.logged = "ng-hide";
 	}
 	$scope.setTimer = function() {
-		if ($scope.si && $scope.auth)
+		if ($scope.si && $scope.auth) {
 			$scope.timer = $timeout($scope.expire, $scope.time);
+			$scope.count = $interval($scope.countdown,1000);
+			$scope.counter = $scope.inp;
+		}
 	}
 	$scope.resetTimer = function() {
 		$timeout.cancel($scope.timer);
+	}
+	$scope.countdown = function() {
+		$scope.counter--;
+		if ($scope.counter == 0) {
+			$scope.counter =  "";
+			$interval.cancel($scope.count);
+		}
 	}
 });
